0ef15f969d0b4a3433e660b9eb397ca6635234e3
ptobechi <ptobechii@gmail.com>
2023-12-18 17:18:46 -0600

    requirement fix

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef _SORT_H
-#define _SORT_H
+#ifndef SORT_H
+#define SORT_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,4 +24,4 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-#endif /** _SORT_H */
+#endif
